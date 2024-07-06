688ed373076a3b81d8ec0569351d86ce8d0371bb
Nataliia Siladi <nataliiasiladi@Nataliias-MBP.fritz.box>
2024-07-06 11:33:06 +0200

    add handling exceptions

--- task2.py
+++ task2.py
@@ -3,19 +3,29 @@ with open('cats_file.txt', 'w') as fh:
     fh.write("60b90c1c13067a15887e1ae1,Tayson,3\n60b90c2413067a15887e1ae2,Vika,1\n60b90c2e13067a15887e1ae3,Barsik,2\n60b90c3b13067a15887e1ae4,Simon,12\n60b90c4613067a15887e1ae5,Tessi,5")
 
 def get_cats_info(path):
-    with  open('cats_file.txt', 'r') as fh:
-        lines = [el.strip() for el in fh.readlines()]
-        cats_info = []
-        cat = {}
+    try:
+        with  open('cats_file.txt', 'r') as fh:
+            lines = [el.strip() for el in fh.readlines()]
+            cats_info = []
+            cat = {}
 
-        for line in lines:
-            new_line = line.split(",")
-            cat["id"] = new_line[0]
-            cat["name"] = new_line[1]
-            cat["age"] = new_line[2]
-            cats_info.append(cat)
+            for line in lines:
+                new_line = line.split(",")
+                cat["id"] = new_line[0]
+                cat["name"] = new_line[1]
+                cat["age"] = new_line[2]
+                cats_info.append(cat)
 
-    return cats_info
+        return cats_info
+    except FileNotFoundError:
+        print("File not found.")
+    except IOError as e:
+        print("An error occurred while reading the file:", e)
+    except Exception as e:
+        print("An unexpected error occurred:", e)
+    
+    
+
 
 cats_info = get_cats_info('cats_file.txt')
 print(cats_info)
